a0928be41ffeeca6461a55898d734f008e4209e8
dengtao6200594 <dengtao6200594@126.com>
2021-09-22 11:24:39 +0800

    测试5

--- src/views/d3js/D3Test.vue
+++ src/views/d3js/D3Test.vue
@@ -1,26 +1,24 @@
 <template>
-  <div class="d3_container">
-    <p>你啊</p>
-    <p>nimade</p>
-  </div>
+	<div class="d3_container">
+		<p>你啊!</p>
+		<p>nimade</p>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "Test",
-  mounted() {
-    let h2 = d3
-    // 不能选择div#app
-      .select(".d3_container")
-      .selectAll("p")
-      .data([1, 2, 3, 4, 5])
-      .text((d) => d)
-      .enter()
-      .append("p")
-      .text((d) => d);
-  },
-};
+	name: 'Test',
+	mounted() {
+		// 不能选择div#app
+		d3.select('.d3_container')
+			.selectAll('p')
+			.data([1, 2, 3, 4, 5])
+			.text(d => d)
+			.enter()
+			.append('p')
+			.text(d => d)
+	}
+}
 </script>
 
-<style>
-</style>
+<style></style>
